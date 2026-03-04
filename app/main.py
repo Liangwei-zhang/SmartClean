2e477f9b1cfb5ac381a5d3a2e0f28f51e4c91b37
Nico <nico@example.com>
2026-03-03 19:28:07 -0700

    chore: 使用舊版前端頁面

--- app/main.py
+++ app/main.py
@@ -87,22 +87,22 @@ from fastapi.responses import FileResponse
 
 @app.get("/")
 async def index_page():
-    return FileResponse("static/cleaner_old.html")
+    return FileResponse("static/cleaner.html")
 
 
 @app.get("/cleaner")
 async def cleaner_page():
-    return FileResponse("static/cleaner_old.html")
+    return FileResponse("static/cleaner.html")
 
 
 @app.get("/host")
 async def host_page():
-    return FileResponse("static/host_old.html")
+    return FileResponse("static/host.html")
 
 
 @app.get("/admin")
 async def admin_page():
-    return FileResponse("static/admin_old.html")
+    return FileResponse("static/admin.html")
 
 
 # 路由
